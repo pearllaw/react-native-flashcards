1247ea241d32b67ab6f6b3d74ff437ef04567528
Pearl Law <pearllynnelaw@gmail.com>
2018-12-31 15:26:02 -0800

    Style score report component #7

--- Flashcards/containers/scoreReport.js
+++ Flashcards/containers/scoreReport.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 
 export default class ScoreReport extends Component {
   static navigationOptions = {
@@ -10,13 +10,19 @@ export default class ScoreReport extends Component {
   }
 
   render() {
-    const { correctCards, incorrectCards } = this.props.screenProps
+    const { correctCards, incorrectCards, flashcards } = this.props.screenProps
+    const percentage = Math.floor((correctCards.length / (flashcards.length) * 100))
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Score Report</Text>
-        <Text>{Math.floor((correctCards.length) / (correctCards.length + incorrectCards.length))} %</Text>
-        <Text>Correct: {correctCards.length}</Text>
-        <Text>Incorrect: {incorrectCards.length} </Text>
+        <Text style={styles.percent}>{percentage} %</Text>
+        <View>
+          <Text style={styles.text}>Correct: {correctCards.length} | Incorrect: {incorrectCards.length}</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Review Incorrect Flashcards</Text>
+          </TouchableOpacity>
+        </View>
+        
       </View>
     )
   }
@@ -25,11 +31,30 @@ export default class ScoreReport extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#00e5ff',
   },
   title: {
-    fontSize: 20
+    fontSize: 22
+  },
+  percent: {
+    fontSize: 60
+  },
+  text: {
+    fontSize: 15,
+    padding: 5,
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#1b1b1b',
+    borderRadius: 25,
+    padding: 12,
+    marginTop: 20
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: '500',
+    textAlign: 'center'
   }
 })
